48f9757a3dd9d172e1d960c6eedd9aa70e143608
Kanhaiya Suthar <91655658+kanhaiyasuthar0@users.noreply.github.com>
2022-03-07 18:41:43 +0530

    Delete js directory

--- js/index.js
+++
@@ -1,9 +0,0 @@
-import navbar from "../components/navbar.js";
-import chatbox from "../components/chatbox.js"
-//  console.log(navbar)
-let header = document.getElementById("header");
-header.innerHTML = navbar();
-// console.log("hello")
-let box = document.createElement("div");
-box.innerHTML = chatbox();
-document.body.append(box);
--- js/payment.js
+++
@@ -1,5 +0,0 @@
-let name = document.getElementById("name");
-let usr = JSON.parse(localStorage.getItem("userdetails"))
-name.value = `${usr.name}`
-
-// console.log(usr)
